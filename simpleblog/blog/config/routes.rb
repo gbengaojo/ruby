eabcafdd91210e1fd7517ac656f1227cb919b26d
Gbenga Ojo <gbenga@gbengaojo.com>
2015-11-16 14:48:21 -0600

    adding articles resource

--- simpleblog/blog/config/routes.rb
+++ simpleblog/blog/config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  Rails.application.routes.draw do
+    resources :articles
+
+    root 'welcome#index'
+  end
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
