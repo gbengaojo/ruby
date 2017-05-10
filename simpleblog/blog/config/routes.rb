e00d7abf594d850f9f7ba7169b18779b92ffc7a3
Gbenga Ojo <gbenga@gbengaojo.com>
2017-05-10 16:53:20 -0500

    show articles; private access modifier for input whitelist

--- simpleblog/blog/config/routes.rb
+++ simpleblog/blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -13,11 +15,9 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  Rails.application.routes.draw do
-    resources :articles
+  resources :articles
 
-    root 'welcome#index'
-  end
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
