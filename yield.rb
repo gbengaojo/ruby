6bc661eed584467a22e828c0cb60b55804a05983
Gbenga Ojo <gbenga@gbengaojo.com>
2017-05-16 21:54:06 -0500

    error in example, missing puts for output

--- yield.rb
+++ yield.rb
@@ -8,4 +8,16 @@ my_method { puts "reached yield" }
 
 ########################
 puts "\n\n"
-puts "hello";
+def my_map(array)
+   new_array = []
+
+   for element in array
+      new_array.push yield element
+   end
+
+   puts new_array
+end
+
+my_map([1, 2, 3]) do |number|
+   number * 2
+end
