3e3aab1a33536b7085b967c378624b191cb75204
Gbenga Ojo <gbenga@gbengaojo.com>
2019-07-16 03:46:50 -0500

    client class constructor

--- sockets/client.rb
+++ sockets/client.rb
@@ -2,7 +2,12 @@ require 'socket'
 
 class Client
   def initialize(socket)
+    @socket = socket
+    @request_object = send_request
+    @response_object = listen_response
 
+    @request_object.join # will send the request to the server
+    @response_object.join # will receive response from the server
   end
 
   def send_request
